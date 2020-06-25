7946d4a04e8d97c80394391b495f676dc38084df
edenildo01 <edenildosouza.fontes@gmail.com>
2020-06-25 16:00:33 -0300

    Projeto Criado

---
+++ src/test/java/com/edenildo/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edenildo.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
