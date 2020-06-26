27ee1fc81ef01c0f950949cae6ae165d11447fa4
edenildo01 <edenildosouza.fontes@gmail.com>
2020-06-26 14:40:05 -0300

    Testando a Primeira Classe de Domínio - Categoria

--- src/main/java/com/edenildo/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/edenildo/cursomc/resource/CategoriaResource.java
@@ -1,14 +1,26 @@
 package com.edenildo.cursomc.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.edenildo.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
   @RequestMapping(method=RequestMethod.GET)
-  public String Listar() {
-	  return "REST Está Funcionand!";
+  public List<Categoria> Listar() {
+	  Categoria cat1 = new Categoria(1,"Informática");
+	  Categoria cat2 = new Categoria(2,"Escritório");
+	  
+	  List<Categoria> Lista = new ArrayList<>(); 
+	  Lista.add(cat1);
+	  Lista.add(cat2);
+	  
+	  return Lista;
   }
 }
